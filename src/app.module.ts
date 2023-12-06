afcb150f598158d4c7a914af1af341c0a67a40c5
jame12945 <jame12945@gmail.com>
2023-12-06 11:06:13 +0700

    Create Table And Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodosModule } from './todos/todos.module';
+import { Todo } from './todos/todo.entity';
 //Decorator is Module
 //Decorator คือ expression ที่จะคืนค่าเป็น function. สังเกตุมี @ นำหน้า
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Todo],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TodosModule, 
+  ],
   //controllers
   controllers: [AppController],
   //services
-  providers: [AppService],
+  providers: [AppService]
+  
 })
 export class AppModule {}
